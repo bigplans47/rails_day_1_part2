82fa171b1ca6c5e1f9fd8eb3e6e795a3301dffae
Guest User <Guest@Epicodus-3A.local>
2018-04-17 15:40:01 -0700

    update site to create new course objects and handle errors of objects without names

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,5 +3,35 @@ class CoursesController < ApplicationController
     @courses = Course.all
     render :index
   end
-  
+
+  # def show
+  #   @course = Course.find(params[:id])
+  #   render :show
+  # end
+  def show
+    @course = Course.find(params[:id])
+    @param = params
+    puts params, ' here 123'
+    render :show
+  end
+
+  def new
+    @course = Course.new
+    render :new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:name)
+  end
+
 end
